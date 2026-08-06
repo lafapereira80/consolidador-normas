cebda3bee3a2a3523496f5aee132dc5b018a8822
lafapereira80 <lafapereira@gmail.com>
2026-08-06 13:33:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def analisar_normas_com_gemini_dinamico(texto_original, texto_alterador, key):
     
     # CORREÇÃO: Utilizando a versão mais atualizada e liberada pelo Google
     response = client.models.generate_content(
-        model='gemini-1.5-pro',
+        model='gemini-2.0-flash',
         contents=prompt,
         config=types.GenerateContentConfig(
             response_mime_type="application/json",
